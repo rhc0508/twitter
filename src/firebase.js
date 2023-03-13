2c56d8909b0a1b81174d58b9aed1f9dea73f3450
rhc0508 <rhc0508@naver.com>
2023-03-13 16:32:45 +0900

    second commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdc9J0mXsGQWANTBN6yHMNlVrPZETbTSI",
+  authDomain: "twitter-69a11.firebaseapp.com",
+  projectId: "twitter-69a11",
+  storageBucket: "twitter-69a11.appspot.com",
+  messagingSenderId: "703288133712",
+  appId: "1:703288133712:web:465df7f151f64769062955",
+};
+
+export default firebase.initializeApp(firebaseConfig);
